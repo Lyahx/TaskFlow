08f600608f6e54c23f5dc22d0cae8477dfc61322
furkanozturk <monsterfurkan33@gmail.com>
2026-03-25 15:46:40 +0300

    Repositories Added

--- TaskFlow.DataAccess/Repositories/Concrete/GenericRepository.cs
+++ TaskFlow.DataAccess/Repositories/Concrete/GenericRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace TaskFlow.DataAccess.Repositories.Concrete;
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.DataAccess.Context;
+using TaskFlow.DataAccess.Repositories.Interfaces;
 
-public class GenericRepository
+namespace TaskFlow.DataAccess.Repositories.Concrete;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
 {
-    
+    private readonly AppDbContext _context;
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<T>> GetAllAsync()
+    {
+        return _context.Set<T>().ToListAsync();
+    }
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        return _context.Set<T>().FindAsync(id).AsTask();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity).AsTask();
+        await _context.SaveChangesAsync();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        return _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
 }
--- TaskFlow.DataAccess/Repositories/Concrete/TaskRepository.cs
+++ TaskFlow.DataAccess/Repositories/Concrete/TaskRepository.cs
@@ -1,6 +1,26 @@
-﻿namespace TaskFlow.DataAccess.Repositories.Concrete;
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.DataAccess.Context;
+using TaskFlow.DataAccess.Repositories.Interfaces;
+using TaskFlow.Entities.Models;
 
-public class TaskRepository
+namespace TaskFlow.DataAccess.Repositories.Concrete;
+
+public class TaskRepository : GenericRepository<AppTask>, ITaskRepository
 {
-    
+    private readonly AppDbContext _context;
+
+    public TaskRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public Task<List<AppTask>> GetByUserIdAsync(int userId)
+    {
+        return _context.AppTasks.Where(x => x.UserId == userId).ToListAsync();
+    }
+
+    public Task<List<AppTask>> GetCompletedTasksAsync(int userId)
+    {
+        return _context.AppTasks.Where(x => x.UserId == userId && x.IsCompleted).ToListAsync();
+    }
 }
